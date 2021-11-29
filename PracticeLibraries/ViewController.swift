df26d97025b4e36befffd03446df072e91ec1556
0102johnny <rt2824.recruit.1022@gmail.com>
2021-11-29 17:47:48 +0900

    no message

---
+++ PracticeLibraries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PracticeLibraries
+//
+//  Created by 戸田　龍馬 on 2021/11/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
