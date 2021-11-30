590707fb7ccc8356e4faa313560391c306301e4f
0102johnny <rt2824.recruit.1022@gmail.com>
2021-11-30 19:03:27 +0900

    既存のIQKeyboardManagerや、後々作成するAlamofireとかRSwiftのテストアプリのStoryboardとSwiftファイルを各々まとめるファイルを追加

---
+++ PracticeLibraries/Kingfisher/KingfisherViewController.swift
@@ -0,0 +1,29 @@
+//
+//  KingfisherViewController.swift
+//  
+//
+//  Created by 戸田　龍馬 on 2021/11/30.
+//
+
+import UIKit
+
+class KingfisherViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
